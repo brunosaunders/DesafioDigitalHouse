efabbf3a320d6b8a2180b9ff2c1bb24d1d0442b7
Bruno Saunders <senabruno97@hotmail.com>
2020-09-25 21:05:47 -0300

    Parte_B Classe Curso

---
+++ src/br/com/digitalhouse/Parte_B/Curso.kt
@@ -0,0 +1,18 @@
+package br.com.digitalhouse.Parte_B
+
+class Curso(val nome: String, val codigo: Int) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Curso
+
+        if (codigo != other.codigo) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return codigo
+    }
+}
