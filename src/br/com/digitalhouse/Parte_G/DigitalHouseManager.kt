12f49d2a7bbfa18a62450696ab03b8bcc18319cd
Bruno Saunders <senabruno97@hotmail.com>
2020-09-25 22:30:20 -0300

    Parte_G Classe DigitalHouseManager

---
+++ src/br/com/digitalhouse/Parte_G/DigitalHouseManager.kt
@@ -0,0 +1,13 @@
+package br.com.digitalhouse.Parte_G
+
+import br.com.digitalhouse.Parte_A.Aluno
+import br.com.digitalhouse.Parte_D.Professor
+import br.com.digitalhouse.Parte_E.Curso
+import br.com.digitalhouse.Parte_F.Matricula
+
+class DigitalHouseManager {
+    val listaAlunos = mutableListOf<Aluno>()
+    val listaProfessores = mutableListOf<Professor>()
+    val listaCursos = mutableListOf<Curso>()
+    val listaMatriculas = mutableListOf<Matricula>()
+}
